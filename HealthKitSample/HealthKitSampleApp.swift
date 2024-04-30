c6d153a10d262a3e7852ddaad34323f9f6c05975
[muranakar] <[P1211065@gmail.com]>
2024-04-30 12:32:34 +0900

    Initial Commit

---
+++ HealthKitSample/HealthKitSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitSampleApp.swift
+//  HealthKitSample
+//
+//  Created by ryo muranaka on 2024/04/30.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
