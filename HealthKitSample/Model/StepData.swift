58181ef3f88961c57dd637a7364f895bd16b5ffa
[muranakar] <[P1211065@gmail.com]>
2024-05-12 22:01:06 +0900

    add StepData Model

---
+++ HealthKitSample/Model/StepData.swift
@@ -0,0 +1,14 @@
+//
+//  StepData.swift
+//  HealthKitSample
+//
+//  Created by ryo muranaka on 2024/05/12.
+//
+
+import Foundation
+
+struct StepData {
+    var stepCount: Int
+    var startDate: Date
+    var endDate: Date
+}
